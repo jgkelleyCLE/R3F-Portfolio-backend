c2c83e5ad6a8d260aca75540d371eee5f51dd068
Jack <74841896+jgkelleyCLE@users.noreply.github.com>
2025-04-24 09:07:55 -0400

    contact controller update

--- controllers/Contact.js
+++ controllers/Contact.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer'
 
 export const sendContact = async(req, res) => {
 
-    const { name, email, message } = req.body
+    const { name, email, phone, message, busName, busCity, busState, busZip, busType, busYears, fedId, landName, landNumber, landYears, landFootage } = req.body
 
    
 
@@ -28,9 +28,21 @@ export const sendContact = async(req, res) => {
         
         <h3>New message from 21600 Miles Road.</h3>
         <h4>Name: ${name}</h4>
-        <h4>From: ${email}</h4>
+        <h4>Email: ${email}</h4>
+        <h4>Phone: ${phone}</h4>
+
+        <h4>Business Name: ${busName}</h4>
+        <h4>Location: ${busCity}, ${busState} ${busZip}</h4>
+        <h4>Business Type: ${busType}</h4>
+        <h4>Years in Business: ${busYears}</h4>
+        <h4>Federal ID: ${fedId}</h4>
+
+        <h4>Previous Landlord Name: ${landName}</h4>
+        <h4>Previous Landlord Number: ${landNumber}</h4>
+        <h4>Years at Property: ${landYears}</h4>
+        <h4>Square Footage of Property: ${landFootage}</h4>
         
-        <h4>Message: ${message}</h4>
+        <h4>Comments: ${message}</h4>
 
       </body>
          
